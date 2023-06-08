aa8100d49678adc5f7146fc9fa1c2f9a7cca164c
Samuli Penttila <samuli@vaadin.com>
2023-06-08 13:17:15 +0300

    Add dummy data and connect it to Grid

--- frontend/views/helloreact/HelloReactView.tsx
+++ frontend/views/helloreact/HelloReactView.tsx
@@ -1,12 +1,20 @@
 import { Button } from '@hilla/react-components/Button.js';
 import { Notification } from '@hilla/react-components/Notification.js';
 import { TextField } from '@hilla/react-components/TextField.js';
-import { HelloReactEndpoint } from 'Frontend/generated/endpoints.js';
+import {HelloReactEndpoint, ShepherdClientEndpoint} from 'Frontend/generated/endpoints.js';
 import { useState } from 'react';
+import {Grid} from "@hilla/react-components/Grid";
+import {GridColumn} from "@hilla/react-components/GridColumn";
+import Data from "Frontend/generated/com/github/taefi/shepherdui/endpoints/shephered/ShepherdClientEndpoint/Data";
+
 
 export default function HelloReactView() {
   const [name, setName] = useState('');
+  const [data, setData] = useState<Data[]>(new Array<Data>());
 
+  useState(async () => {
+      setData(await ShepherdClientEndpoint.getData());
+  })
   return (
     <>
       <section className="flex p-m gap-m items-end">
@@ -19,12 +27,20 @@ export default function HelloReactView() {
         <Button
           onClick={async () => {
             const serverResponse = await HelloReactEndpoint.sayHello(name);
+
+            setData(((await ShepherdClientEndpoint.getData())));
             Notification.show(serverResponse);
           }}
         >
           Say hello
         </Button>
+
+          <Grid items={data}>
+              <GridColumn path="name"></GridColumn>
+              <GridColumn path="org"></GridColumn>
+          </Grid>
       </section>
     </>
   );
+
 }
--- src/main/java/com/github/taefi/shepherdui/endpoints/shephered/ShepherdClientEndpoint.java
+++ src/main/java/com/github/taefi/shepherdui/endpoints/shephered/ShepherdClientEndpoint.java
@@ -7,6 +7,8 @@ import dev.hilla.Nonnull;
 import jakarta.annotation.security.PermitAll;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Endpoint
 @PermitAll
@@ -23,4 +25,14 @@ public class ShepherdClientEndpoint {
         return shepherdClient.getAllProjects(null);
     }
 
+    @Nonnull
+    public List<@Nonnull Data> getData() {
+        return IntStream.range(0, 100).mapToObj(i -> new Data("name %d".formatted(i), "org %d".formatted(i))).collect(Collectors.toList());
+    }
+
+    public record Data(
+            String name,
+            String org
+    ) {
+    }
 }
