47d4ff9c7e5d6517718e6d0182d02b1a07e07dd5
Samuli Penttila <samuli@vaadin.com>
2023-06-08 16:21:54 +0300

    More columns to grid and fields to form.

--- frontend/views/helloreact/HelloReactView.tsx
+++ frontend/views/helloreact/HelloReactView.tsx
@@ -5,55 +5,97 @@ import {HelloReactEndpoint, ShepherdClientEndpoint} from 'Frontend/generated/end
 import { useState } from 'react';
 import {Grid} from "@hilla/react-components/Grid";
 import {GridColumn} from "@hilla/react-components/GridColumn";
-import Data from "Frontend/generated/com/github/taefi/shepherdui/endpoints/shephered/ShepherdClientEndpoint/Data";
 import ProjectView from "Frontend/generated/com/github/taefi/shepherdui/endpoints/shephered/dto/ProjectView";
 import Project from "Frontend/generated/com/github/taefi/shepherdui/endpoints/shephered/dto/Project";
+import gitRepo from "Frontend/generated/com/github/taefi/shepherdui/endpoints/shephered/dto/GitRepo";
 
 
 export default function HelloReactView() {
-  const [name, setName] = useState('');
+  //Project DTO to be modified by the form.
   const [project, setProject] = useState<Project>({});
+
+  //Project properties
   const [projectDescription, setProjectDescription] = useState<string>('');
+  const [projectWebpage, setProjectWebpage] = useState<string>('');
+  const [gitUrl, setGitUrl] = useState<string>('');
+  const [gitBranch, setGitBranch] = useState<string>('');
+
+  //For grid
   const [projects, setProjects] = useState<ProjectView[]>(new Array<ProjectView>());
   const [selectedProject, setSelectedProject] = useState<ProjectView[]>(new Array<ProjectView>());
 
   useState(async () => {
-      setProjects(await ShepherdClientEndpoint.getProjects('samuli@vaadin.com'));
+      setProjects(await ShepherdClientEndpoint.getProjects(''));
   })
 
     return (
-    <>
-      <section className="flex p-m gap-m items-end">
+    <section className="flex flex-row">
+      <section className="flex flex-col p-m gap-m items-end">
         <TextField
             value={projectDescription}
-          label="Your name"
+            onValueChanged={e => setProjectDescription(e.detail.value)}
+          label="Project description"
         />
+          <TextField
+              value={projectWebpage}
+              onValueChanged={e => setProjectWebpage(e.detail.value)}
+              label="Webpage"
+          />
+          <TextField
+              value={gitUrl}
+              onValueChanged={e => setGitUrl(e.detail.value)}
+              label="Git url"
+          />
+
+          <TextField
+              value={gitBranch}
+              onValueChanged={e => setGitBranch(e.detail.value)}
+              label="Git branch"
+          />
         <Button
           onClick={async () => {
               project.description = projectDescription;
-              console.log('project.description='+project.description);
-              console.log('projectDescription='+projectDescription);
+              project.webpage = projectWebpage;
+              if (project.gitRepo == undefined) {
+                  project.gitRepo = {};
+              }
+              project.gitRepo.branch = gitBranch;
+              project.gitRepo.url = gitUrl;
               await ShepherdClientEndpoint.createProject(project);
           }}
         >
-          Say hello
+          Create project
         </Button>
-
-          <Grid items={projects}
-                selectedItems={selectedProject}
-                onActiveItemChanged={({ detail: { value } }) => {
-                    setProject(value ? value.project ? value.project : {} : {});
-                    setProjectDescription(project.description?project.description:'');
-                    console.log('changed selected items' + project.description);
-                }
-                }
-          >
-              <GridColumn header="Project">
-                  {({ item }) => <>{item.project.description}</>}
-              </GridColumn>
-          </Grid>
       </section>
-    </>
+
+      <Grid items={projects}
+            selectedItems={selectedProject}
+            onActiveItemChanged={({ detail: { value } }) => {
+                    setProject(value ? value.project ? value.project : {} : {});
+                    setProjectDescription(project?.description || '');
+                    setProjectWebpage(project?.webpage || '');
+                    setGitUrl(project?.gitRepo?.url || '');
+                    setGitBranch(project?.gitRepo?.branch || '');
+                }
+            }
+      >
+          <GridColumn header="Project">
+              {({ item }) => <>{item.project.description}</>}
+          </GridColumn>
+
+          <GridColumn header="Webpage">
+              {({ item }) => <>{item.project.webpage}</>}
+          </GridColumn>
+
+          <GridColumn header="Owner">
+              {({ item }) => <>{item.project.owner?.name} {item.project.owner?.email}</>}
+          </GridColumn>
+
+          <GridColumn header="Gitrepo">
+              {({ item }) => <>{item.project.gitRepo?.url} {item.project.gitRepo?.branch}</>}
+          </GridColumn>
+      </Grid>
+    </section>
   );
 
 }
